073cbe748e81f04c9e77d00f69cca08349950147
JerryEnroute <gerardo.partida@enroutesystems.com>
2023-08-28 20:06:21 -0600

    forgot to make dags pull and merge multiple csv files

--- dags/sales_scripts/get_data.py
+++ dags/sales_scripts/get_data.py
@@ -1,20 +1,34 @@
 import os
 import pathlib
-
 import pandas as pd
 
-
 def main():
-    # Reads CSV data
+    # Define paths
     AIRFLOW_HOME = os.getenv('AIRFLOW_HOME')
-    directory = pathlib.Path(AIRFLOW_HOME, "scrapper", "data", "sales", "2023", "08", "22")
-    input_files = os.listdir(f"{directory}/")
-    destination_files = []
-    for input_file in input_files:
-        destination_filename = "sales_etl.csv"
-        print(input_file, "mapped to", destination_filename)
-        raw_destination = pathlib.Path(AIRFLOW_HOME, "data_warehouse", "data", destination_filename)
-        df = pd.read_csv(os.path.join(AIRFLOW_HOME, "scrapper", "data", "sales", "2023", "08", "22", input_file))
-        df.to_csv(raw_destination)
-        destination_files.append(destination_filename)
-    return destination_files
+    directory = pathlib.Path(AIRFLOW_HOME, "scrapper", "data", "sales")
+    
+    # Create an empty list to store individual dataframes
+    dfs = []
+
+    # Iterate through subdirectories and fetch CSV files
+    for subdir, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".csv"):
+                filepath = os.path.join(subdir, file)
+                df = pd.read_csv(filepath)
+                dfs.append(df)
+                print(f"Read {filepath}")
+
+    # Concatenate all the dataframes into one
+    combined_df = pd.concat(dfs, ignore_index=True)
+    
+    # Save the final dataframe to sales_etl.csv
+    destination_filename = "sales_etl.csv"
+    raw_destination = pathlib.Path(AIRFLOW_HOME, "data_warehouse", "data", destination_filename)
+    combined_df.to_csv(raw_destination, index=False)
+    print(f"Data merged and saved to {destination_filename}")
+
+    return [destination_filename]
+
+if __name__ == '__main__':
+    main()
